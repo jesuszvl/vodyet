4cb7b68605cd9439a82533339d86bab196c431cf
jesuszvl <jesus.zavala@gametime.co>
2023-01-03 17:47:31 -0800

    add icons

--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -1,10 +1,36 @@
 import styles from "./Record.module.scss";
 import classNames from "classnames";
+import HouseIcon from "../../icons/HouseIcon";
+import FoodIcon from "../../icons/FoodIcon";
+import CarIcon from "../../icons/CarIcon";
+import BankIcon from "../../icons/BankIcon";
+import BulbIcon from "../../icons/BulbIcon";
+import GiftIcon from "../../icons/GiftIcon";
 
 export default function Record({ description, ammount, date, category }) {
+  const renderIcon = () => {
+    switch (category) {
+      case "vivienda":
+        return <HouseIcon />;
+      case "comida":
+        return <FoodIcon />;
+      case "transporte":
+        return <CarIcon />;
+      case "deudas":
+        return <BankIcon />;
+      case "servicios":
+        return <BulbIcon />;
+      case "ocio":
+        return <GiftIcon />;
+      default:
+        return <HouseIcon />;
+    }
+  };
   return (
     <div className={styles.record}>
-      <span className={classNames(styles["icon"], styles[category])}></span>
+      <span className={classNames(styles["icon"], styles[category])}>
+        {renderIcon()}
+      </span>
       <span className={styles.description}>{description}</span>
       <span className={styles.ammount}>{ammount}</span>
     </div>
