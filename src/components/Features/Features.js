2e9c5d52632711c9fe3bc8c3ceefb2f137609b3c
jesuszvl <jesus.zavala@gametime.co>
2023-04-30 18:02:10 -0700

    add features content

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -6,19 +6,20 @@ import styles from "./Features.module.scss";
 const features = [
   {
     title: "Gasta",
-    description: "Captura tus gastos diarios de manera categórica",
+    description:
+      "Con nuestra herramienta de administración de gastos, registra tus transacciones diarias de manera organizada y categórica. Mantén un registro completo de tus finanzas y conoce en qué áreas estás invirtiendo tus recursos al visualizar tus gastos por categoría.",
     icon: <MoneyIcon />,
   },
   {
     title: "Rastrea",
     description:
-      "Obten información relevante sobre tu presupuesto mensual y descubre hacia donde se va tu dinero",
+      " Obtén información relevante sobre tu presupuesto mensual y descubre hacia dónde se va tu dinero con nuestra herramienta de seguimiento financiero. Analiza tus patrones de consumo al tener acceso a información detallada sobre tus gastos y ajusta tu presupuesto para alcanzar tus objetivos financieros.",
     icon: <ChartIcon />,
   },
   {
     title: "Planea",
     description:
-      "Establece metas financieras y desarrolla un plan para lograrlo acompañado de nuestra asistente IA",
+      "Establece tus metas financieras y crea un plan personalizado con nuestra herramienta de planificación financiera. Con la ayuda de nuestra asistente IA, desarrolla un plan que te ayude a alcanzar tus objetivos y optimizar tus finanzas para hacer realidad tus sueños y alcanzar la libertad financiera.",
     icon: <WalletIcon />,
   },
 ];
@@ -27,17 +28,19 @@ export default function Features() {
   return (
     <div className={styles["features-container"]}>
       <h1 className={styles["features-title"]}>¿Como funciona?</h1>
-      {features.map(({ title, description, icon }) => {
-        return (
-          <div key={title} className={styles["feature"]}>
-            <div className={styles["feature-main"]}>
-              {icon}
-              <span className={styles.title}>{title}</span>
+      <div className={styles["features-grid"]}>
+        {features.map(({ title, description, icon }) => {
+          return (
+            <div key={title} className={styles["feature"]}>
+              <div className={styles["feature-main"]}>
+                {icon}
+                <span className={styles.title}>{title}</span>
+              </div>
+              <p className={styles.description}>{description}</p>
             </div>
-            <p className={styles.description}>{description}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
